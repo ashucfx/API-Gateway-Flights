f407dd930031ce10f735eeb6e94aa15e92970e25
Ashutosh Shukla <ashutosh6471@gmail.com>
2026-02-28 22:03:06 +0530

    fix(api-gateway): correct typo in isAdmin middleware

--- src/middlewares/auth-request-middleware.js
+++ src/middlewares/auth-request-middleware.js
@@ -1,52 +1,52 @@
-const {StatusCodes} = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 
-const {SuccessResponse,ErrorResponse} = require('../utils/common');
+const { SuccessResponse, ErrorResponse } = require('../utils/common');
 const AppError = require('../utils/errors/app-error');
-const {UserService} = require('../services');
+const { UserService } = require('../services');
 
-function validateAuthRequest(req,res,next){
-    if (!req.body.email){
+function validateAuthRequest(req, res, next) {
+    if (!req.body.email) {
         ErrorResponse.message = 'Something went wrong while authenticating user';
         ErrorResponse.error = new AppError(['Email not found in the incoming request in the correct form'])
         return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json(ErrorResponse);
-        
+            .status(StatusCodes.BAD_REQUEST)
+            .json(ErrorResponse);
+
     }
-    if (!req.body.password){
+    if (!req.body.password) {
         ErrorResponse.message = 'Something went wrong while authenticating user';
         ErrorResponse.error = new AppError(['Password not found in the incoming request in the correct form'])
         return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json(ErrorResponse);
-        
+            .status(StatusCodes.BAD_REQUEST)
+            .json(ErrorResponse);
+
     }
     next();
 }
 
-async function checkAuth(req,res,next){
+async function checkAuth(req, res, next) {
     try {
         const response = await UserService.isAuthenticated(req.headers['x-access-token']);
-        if(response){
+        if (response) {
             req.user = response; //setting the user id in the req object
             next();
         }
-        
+
     } catch (error) {
         return res
             .status(error.statusCode)
             .json(error);
 
-        
+
     }
 }
 
-async function isAdmin(req,res,next){
-    const reponse = await UserService.isAdmin(req.user);
-    if (!reponse){
+async function isAdmin(req, res, next) {
+    const response = await UserService.isAdmin(req.user);
+    if (!response) {
         return res
-        .status(StatusCodes.UNAUTHORIZED)
-        .json({message:'User not authorized for this action'});
+            .status(StatusCodes.UNAUTHORIZED)
+            .json({ message: 'User not authorized for this action' });
 
     }
     next();
